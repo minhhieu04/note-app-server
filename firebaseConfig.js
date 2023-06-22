ba0b13d8bdd0623b010827aa0fcebb287da27ce6
minhhieu04 <minhhieu.tran.mcs@gmail.com>
2023-06-22 14:49:57 +0700

    Create repo note-app-server

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase-admin/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdMwVlp0_Zt6UUSCigZpYChIlnZuftXAU",
+  authDomain: "note-app-f7f01.firebaseapp.com",
+  projectId: "note-app-f7f01",
+  storageBucket: "note-app-f7f01.appspot.com",
+  messagingSenderId: "836638932128",
+  appId: "1:836638932128:web:bdf5038ede48bfd8ba5dc5",
+  measurementId: "G-JY311NT4NH",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
